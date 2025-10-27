c7d5ade18dbc5bd30e99e162fd9af6c182928a19
omergullu <omergullu555@gmail.com>
2025-10-28 00:05:52 +0300

    partialview lerin yarısı oluşturuldu

---
+++ ECommerceApp.WebUI/ViewComponents/Default/_SliderForBestSellingPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceApp.WebUI.ViewComponents.Default
+{
+    public class _SliderForBestSellingPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
