361e9841f56640dd846d49d4548f45cd1da02dec
omergullu <omergullu555@gmail.com>
2025-10-30 23:57:34 +0300

    eklenen entitylere Repository design pattern kısmi olarak başlandı

---
+++ ECommerceApp.Core/Interfaces/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Core.Interfaces
+{
+    public interface IBasketRepository : IRepository<Basket>
+    {
+    }
+}
